1b33a23053177cc24e6b21ed88cc539fef3c2b52
madhurop <madhurborade654@gmail.com>
2024-05-12 20:05:35 +0530

    OK

--- src/Comoponents/Navbar.jsx
+++ src/Comoponents/Navbar.jsx
@@ -5,14 +5,14 @@ function Navbar() {
     <div className="h-14 w-full flex flex-row justify-between items-center p-4  ">
         <h1 className=" text-white text-3xl  ">Madhur</h1>
         <div className=" flex flex-row w-6/12 h-full justify-evenly items-center navHide">
-            <div className="text-white hover:text-orange-400  hover:border-b border-orange-400">Home</div>
-            <div className="text-white hover:text-orange-400  hover:border-b border-orange-400">About</div>
-            <div className="text-white hover:text-orange-400  hover:border-b border-orange-400">Resume</div>
-            <div className="text-white hover:text-orange-400  hover:border-b border-orange-400">Service</div>
-            <div className="text-white hover:text-orange-400  hover:border-b border-orange-400">Skills</div>
-            <div className="text-white hover:text-orange-400  hover:border-b border-orange-400">Project</div>
-            <div className="text-white hover:text-orange-400  hover:border-b border-orange-400">My Blog</div>
-            <div className="text-white hover:text-orange-400  hover:border-b border-orange-400">Contact</div>
+            <div className="text-black hover:text-orange-400  hover:border-b border-orange-400  md:text-white ">Home</div>
+            <div className="text-black hover:text-orange-400  hover:border-b border-orange-400  md:text-white ">About</div>
+            <div className="text-black hover:text-orange-400  hover:border-b border-orange-400  md:text-white ">Resume</div>
+            <div className="text-black hover:text-orange-400  hover:border-b border-orange-400  md:text-white ">Service</div>
+            <div className="text-black hover:text-orange-400  hover:border-b border-orange-400  md:text-white ">Skills</div>
+            <div className="text-black hover:text-orange-400  hover:border-b border-orange-400  md:text-white ">Project</div>
+            <div className="text-black hover:text-orange-400  hover:border-b border-orange-400  md:text-white ">My Blog</div>
+            <div className="text-black hover:text-orange-400  hover:border-b border-orange-400  md:text-white ">Contact</div>
         </div>
       
     </div>
