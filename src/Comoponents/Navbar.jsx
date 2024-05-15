220018b2c0ab85c0f50acb4d6794c6e1258275ec
madhurop <madhurborade654@gmail.com>
2024-05-15 19:30:01 +0530

    Server

--- src/Comoponents/Navbar.jsx
+++ src/Comoponents/Navbar.jsx
@@ -1,16 +1,37 @@
 import React from 'react'
+import { useEffect } from 'react'
 
 function Navbar() {
+   useEffect(()=>{
+    personalApi()
+   },[])
+   
+   const personalApi = async () => {
+    try {
+      const apiUrl = await fetch("/api/v1/products");
+      const res = await apiUrl.json();
+      console.log(res);
+  
+      // Assuming res is already an array of JSON strings
+      // const arr = res.map(e => JSON.parse(e));
+      // console.log(arr);
+      // console.log(arr[1][0]);
+      // console.log(arr[1][0].length);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  
   return (
 
 
     <nav className="bg-black border-gray-200 dark:bg-gray-900">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a className="flex items-center space-x-3 rtl:space-x-reverse">
-          
+
           <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">Madhur</span>
         </a>
-        <button  type="button"  className="inline-flex text-bolder items-center p-2 w-10 h-10 justify-center text-sm border border-white text-white rounded-lg md:hidden bg-black hover:bg-gray-400" aria-controls="navbar-default" aria-expanded="false">
+        <button type="button" className="inline-flex text-bolder items-center p-2 w-10 h-10 justify-center text-sm border border-white text-white rounded-lg md:hidden bg-black hover:bg-gray-400" aria-controls="navbar-default" aria-expanded="false">
           O
         </button>
         <div className="hidden w-full md:block md:w-auto" id="navbar-default navHide">
